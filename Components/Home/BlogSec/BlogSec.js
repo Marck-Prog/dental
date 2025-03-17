c4c465d83a18e02987514f804f68d04919550389
Marck-Prog <marckfriencecoto@gmail.com>
2025-03-17 21:14:02 +0800

    Add BlogSec component to display the latest blog posts with customizable cards and responsive layout

---
+++ Components/Home/BlogSec/BlogSec.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import { Container, Grid, Box } from '@mui/material';
+import WPAPI from 'wpapi';
+import { SectionalHeading, CustomCard } from '../../components';
+
+const BlogSec = ({blogs}) => {
+    return (
+        <>
+            {
+                blogs.length > 0 ? <Container maxWidth="xxl">
+                    <Grid container>
+                        <Grid item xs={12} md={10} className="mx-auto">
+                            <Box >
+                                <SectionalHeading variant={'subtitle1'} align="center" title="LATEST BLOGS" />
+                                <Box py={2}>
+                                    <Grid container spacing={3} justifyContent="center">
+                                        {
+                                            blogs.map(item => <Grid key={item.id} item xs={12} md={6} lg={4}>
+                                                <CustomCard blogMedia={item._embedded['wp:featuredmedia'][0].source_url} cardMediaAlt={item._embedded['wp:featuredmedia'][0].alt_text} cardTitle={item.title.rendered} navlink={true} link={`/blog/${item.slug}/`} cardText={`${item.excerpt.rendered.split(" ").slice(0, 20).join(" ")}[...]`} cardList={null} height="26.8rem" />
+                                            </Grid>)
+                                        }
+                                    </Grid>
+                                </Box>
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Container> : null
+            }
+        </>
+    )
+}
+
+export default BlogSec
