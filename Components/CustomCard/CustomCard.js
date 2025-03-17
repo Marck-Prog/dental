e985f026b4d3487d48e81dcd0ae7c70161b78f12
Marck-Prog <marckfriencecoto@gmail.com>
2025-03-17 21:13:23 +0800

    Add CustomCard component for displaying customizable cards with media, text, and action buttons

---
+++ Components/CustomCard/CustomCard.js
@@ -0,0 +1,58 @@
+import { Card, CardContent, CardMedia, Typography, Box, List, ListItem, ListItemIcon, ListItemText, CardActions } from "@mui/material"
+import Link from "next/link"
+import { Check } from '@mui/icons-material'
+import Styles from '../../styles/Home/Offer.module.css'
+import { useEffect, useState } from "react"
+
+const CustomCard = ({ height, cardMedia, cardMediaAlt, mediaHeight, navlink, anchor, link, cardText, cardTitle, bgColor, cardList, icon, offerBtn, price, small, blogMedia, cls }) => {
+    const [width, setWidth] = useState();
+
+    useEffect(()=>{
+        setWidth(window.innerWidth)
+    },[width])
+
+    return (
+        <>
+            <Card className={`shadow grow ${cls}`} style={{ height: height && width > 900 ? height : "auto", position:"relative" }}>
+                {
+                    cardMedia ? <CardMedia component="img" image={cardMedia.src} alt={cardMediaAlt} height={mediaHeight ? mediaHeight : "200"} /> : null
+                }
+                {
+                    blogMedia ? <CardMedia component="img" image={blogMedia} alt={cardMediaAlt} height={mediaHeight ? mediaHeight : "200"} /> : null
+                }
+                {
+                    cardTitle || cardText || cardList ? <CardContent className={bgColor ? 'isColor' : null}  >
+                        {
+                            cardTitle ? <Typography variant="h5" className="cardHead" gutterBottom>{navlink ? <Link href={link}>
+                                <a>{cardTitle}</a>
+                            </Link> : anchor ? <a href={link}>{cardTitle}</a> : cardTitle}</Typography> : null
+                        }
+                        {
+                            cardText ? <Typography className="para" dangerouslySetInnerHTML={{ __html: cardText }} /> : null
+                        }
+                        {
+                            cardList !== null ? cardList.length > 0 ? <Box mb={3}>
+                                <List>
+                                    {
+                                        cardList.map(item => <ListItem key={item.id}>
+                                            {icon ? <ListItemIcon><Check style={{ color: "#117295" }} /></ListItemIcon> : null}
+                                            <ListItemText primary={item} className="para" />
+                                        </ListItem>)
+                                    }
+                                </List>
+                            </Box> : null : null
+                        }
+                    </CardContent> : null}
+                {
+                    offerBtn ? <CardActions className={`d-block ${Styles.OfferBtnSmall} mt-1`}><Box py={2} style={{ backgroundColor: "var(--navy)", borderRadius: "0.5rem", width:"100%", display:"block" }}>
+                        <Typography align="center" style={{ color: "#fff", fontWeight: "600", fontSize: "1.5rem" }}>{price}</Typography>
+                    </Box>
+                       <Typography className="para font-weight-bold" style={{ color: "#117295" }} align="center">{small}</Typography>
+                    </CardActions> : null
+                }
+            </Card>
+        </>
+    )
+}
+
+export default CustomCard
